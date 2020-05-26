2877699725bdbb40a8b137e38d202fe64fc9bb95
alan <alan.araujosb@gmail.com>
2020-05-25 23:15:58 -0300

    enable doc and delete jade template

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
-var productsRoute = require('./routes/products');
-var ordersRoute = require('./routes/orders');
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "Shop API",
+            description: "The backend Api",
+            contact: {
+                name: 'alan.araujosb@gmail.com'
+            },
+            servers: "http://localhost:4646"
+        }
+    },
+    apis: ["app.js", ".routes/*.js"]
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 var app = express();
-
-app.use('/api/products', productsRoute);
-app.use('/api/orders', ordersRoute);
 
 app.use(cors({
 	origin: "*",
@@ -20,15 +32,14 @@ app.use(cors({
 	allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
 }));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Def routes
+app.use('/api/products', require('./routes/products'));
+app.use('/api/orders', require('./routes/orders'));
 
 module.exports = app;
