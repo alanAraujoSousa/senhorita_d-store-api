426236a856c70a51c506c9f7e5cea5b32628f876
alan <alan.araujosb@gmail.com>
2020-05-26 22:05:28 -0300

    fix the routes mapping

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
+var app = express();
+
+app.use(cors({
+	origin: "*",
+	methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+	allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 
@@ -24,24 +38,10 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-var app = express();
-
-app.use(cors({
-	origin: "*",
-	methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
-	allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
-}));
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Def routes
 app.use('/api/products', require('./routes/products'));
 app.use('/api/orders', require('./routes/orders'));
-app.use('/api/users', require('/routes/users																																																																'));
-app.use('/api/auth', require('/routes/auth'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
 
 module.exports = app;
